a7c18150028a00c66e8beda8e665e6412df84b99
Chris Willmore <chris.willmore@yahoo.com>
2011-09-05 17:43:03 +0300

    Completion of R assignment tasks.

--- hw1/hw1.R
+++ hw1/hw1.R
@@ -2,6 +2,15 @@ dataset <- read.csv(file="01_random.txt",head=TRUE,sep="\t")
 # 1.4 Calculate mean, median, standard deviation, etc of each column 
 summary(dataset)
 
+# 1.5 Plot "X vs Y1"
+plot(dataset$X, dataset$Y1, ylim=c(0,100), main="X vs Y1")
 
+# 1.5 Plot "X vs Y2"
+plot(dataset$X, dataset$Y2, ylim=c(0,300), main="X vs Y2")
 
+# 1.5 Correlaton between X and Y1
+cor(dataset$X, dataset$Y1)
+
+# 1.5 Correlaton between X and Y2
+cor(dataset$X, dataset$Y2)
 
